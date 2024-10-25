998878bac5e4f1d5092548f24a87862a1c095ecf
Hadi Ahmed <hadiahmed554@gmail.com>
2024-10-25 15:24:35 -0400

    Task 2.1

--- src/main/java/data_access/FileUserDataAccessObject.java
+++ src/main/java/data_access/FileUserDataAccessObject.java
@@ -92,6 +92,14 @@ public class FileUserDataAccessObject implements SignupUserDataAccessInterface,
         return accounts.get(username);
     }
 
+    /**
+     * @param name name of cur user.
+     */
+    @Override
+    public void setCurrentUser(String name) {
+
+    }
+
     @Override
     public boolean existsByName(String identifier) {
         return accounts.containsKey(identifier);
--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set the current user.
+     * @param name name of cur user.
+     */
+    void setCurrentUser(String name);
 }
